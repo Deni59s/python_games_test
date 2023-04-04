9ee6c09b544ee4b61276624a5f4dc2acf0755d46
Deni59s <d59enis@gmail.com>
2023-04-04 16:22:51 +0300

    linter errors fixed

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,10 @@
 import prompt
-name=''
+name = ''
+
+
 def welcome_user():
-	print('Welcome to the Brain Games!')
-	name = prompt.string('May I have your name? ')
-	print("Hello,", name, '!')
-	#return name
-#print(welcome_user())
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print("Hello,", name, '!')
+    # return name
+# print (welcome_user())
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from brain_games.cli import welcome_user
 
+
 def main():
-    welcome_user() #вызов фунции с запросом имени из дир-ии brain_games/
-    #print('Welcome to the Brain Games!')# не сработала последовательность, перенес в cli.py
+    welcome_user()  # вызов фунции с запросом имени из дир-ии brain_games/
+    # print('Welcome to the Brain Games!'),
+    # не сработала посл-ть при выводе, перенес в cli.py
+
 
 if __name__ == '__main__':
     main()
-
