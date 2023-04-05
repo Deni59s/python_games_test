dc433ac5620fec48c55249a7b96c397591f02013
Deni59s <d59enis@gmail.com>
2023-04-05 23:27:00 +0300

    game brain-even is work.

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+def main():
+    generate()  # вызов фунции с запросом имени из дир-ии brain_games/
+
+
+if __name__ == '__main__':
+    main()
+
+from random import randint
+
+import prompt
+answer = ''
+lst=[]
+
+
+def generate():
+    name = prompt.string('May I have your name? ')
+    
+    number = 0
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for x in range(3):
+        lst.append(randint(1,1000))
+    for random_number in lst:
+        print('Question:',random_number)
+        answer = prompt.string('Your answer:')
+        if (random_number % 2 ==0 and answer == 'yes') or (random_number % 2 != 0 and answer == 'no'):
+            print('Correct!')
+            number=number + 1
+        elif random_number % 2 != 0 and answer == 'no':
+            print('Correct!')
+            number=number + 1
+        else:
+            print("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, " +name+"!")
+            return
+    if number == 3:
+        print(f'Congratulations, {name}!')
+    else:
+        return None 
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -6,7 +6,7 @@ def main():
     welcome_user()  # вызов фунции с запросом имени из дир-ии brain_games/
     # print('Welcome to the Brain Games!'),
     # не сработала посл-ть при выводе, перенес в cli.py
-
+    return name
 
 if __name__ == '__main__':
     main()
