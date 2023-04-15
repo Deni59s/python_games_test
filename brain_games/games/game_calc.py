61c524bc5a65e4a17a8bb4dc1ffe2515e2eceb6d
Deni59s <d59enis@gmail.com>
2023-04-15 19:34:50 +0300

    не прошел тест, исправил логику работы программы brain-calc, ввел модуль operator

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python3
-import random
+from random import randint, choice
+from operator import add, sub, mul
 
 
 ABOUT = 'What is the result of the expression?'
 
 
 def choice_game():
+    number_1 = randint(1, 100)
+    number_2 = randint(1, 100)
+    operator = choice('-+*')
+
+    if operator == '+':
+        result = add(number_1, number_2)
+    elif operator == '-':
+        result = sub(number_1, number_2)
+    elif operator == '*':
+        result = mul(number_1, number_2)
+
+    question = '{} {} {}'.format(number_1, operator, number_2)
+    correct_answer = str(result)
+
+    return question, correct_answer
+
+
+""" def choice_game():
     lst_number = [random.randint(1, 100) for i in range(2)]
     lst_sign = random.choice(['+', '-', '*'])
     if lst_sign == '+':
@@ -14,7 +33,8 @@ def choice_game():
         game_answer = lst_number[0] - lst_number[1]
     elif lst_sign == '*':
         game_answer = lst_number[0] * lst_number[1]
-    number = f'{lst_number[0]}{lst_sign}{lst_number[1]}'
+    number = '{} {} {}'.format(number1, symbol, number2)
+    #number = f'{lst_number[0]}{lst_sign}{lst_number[1]}'
     return number, str(game_answer)
 
-#  print(choice_game())
+print(choice_game())"""
